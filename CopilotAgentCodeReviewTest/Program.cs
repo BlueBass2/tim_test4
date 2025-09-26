7da6fa4edbffc25dd18df0176d3027e4134bdb34
Paul1_Huang <156768984+BlueBass2@users.noreply.github.com>
2025-09-26 14:20:46 +0800

    Add files via upload

---
+++ CopilotAgentCodeReviewTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopilotAgentCodeReviewTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static float CalculateBMI()
+        {
+            float result = 0;
+            return result;
+        }
+
+        public static void Save(string userInputFileName)
+        {
+        }
+
+        public static DataTable GetUserData(string userInput, SqlConnection connection)
+        {
+            DataTable dt = new DataTable();
+            return dt;
+        }
+    }
+}
