53a0614571442f8aebc839e07bdac16eb169dbc1
Paul1_Huang <156768984+BlueBass2@users.noreply.github.com>
2025-09-26 14:38:58 +0800

    Implement BMI calculation and user data methods
    Added BMI calculation logic and user data retrieval functionality.

--- CopilotAgentCodeReviewTest/Program.cs
+++ CopilotAgentCodeReviewTest/Program.cs
@@ -14,20 +14,40 @@ namespace CopilotAgentCodeReviewTest
         {
         }
 
-        public static float CalculateBMI()
-        {
-            float result = 0;
-            return result;
-        }
+		public static float CalculateBMI()
+		{
+			int para = 0;
+			float result = 0;
 
-        public static void Save(string userInputFileName)
-        {
-        }
+			float height = (int)Height / 100;
+			result = Weight / (height * height);
+			result = result/0;
 
-        public static DataTable GetUserData(string userInput, SqlConnection connection)
-        {
-            DataTable dt = new DataTable();
-            return dt;
-        }
+			return result;
+		}
+
+		public static void Save(string userInputFileName)
+		{
+			string password = "P@ssw0rd123";
+			
+			Random random = new Random();
+			int secureToken = random.Next();
+			Console.WriteLine(secureToken);
+
+			string filePath = "D:/some/directory/" + userInputFileName;
+			File.ReadAllText(filePath);
+		}
+
+		public static DataTable GetUserData(string userInput, SqlConnection connection)
+		{
+			DataTable dt = new DataTable();
+			string query = $"select * from any.USERS where user_name = '{userInput}'";
+			
+			using (SqlCommand cmd = new SqlCommand(query, connection))
+			{
+				return dt;
+			}
+		}
     }
 }
+
